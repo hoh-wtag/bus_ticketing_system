044db4ce157c8b483afc6fffa66f3a6d1ef42209
Hasibul Hoque Chowdhury <hasibul.hoque@welldev.io>
2023-04-30 14:13:29 +0600

    Added Tests for Bus model
    Added 9 tests for Bus model.

--- spec/factories/buses.rb
+++ spec/factories/buses.rb
@@ -1,8 +1,8 @@
 FactoryBot.define do
   factory :bus do
-    bus_code { "MyString" }
-    bus_type { 1 }
-    bus_company { "MyString" }
-    bus_capacity { 1 }
+    bus_code { Faker::Vehicle.singapore_license_plate }
+    bus_type { %i[ac non_ac].sample }
+    bus_company { Faker::Vehicle.manufacture }
+    bus_capacity { Faker::Number.between(from: 1, to: 60) }
   end
 end
--- spec/models/bus_spec.rb
+++ spec/models/bus_spec.rb
@@ -1,5 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe Bus, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let!(:bus) { FactoryBot.create(:bus) }
+  describe 'when creating a bus' do
+    it 'has a valid factory' do
+      expect(bus).to be_valid
+    end
+    it { should validate_presence_of(:bus_code) }
+    it { should validate_presence_of(:bus_type) }
+    it { should validate_presence_of(:bus_company) }
+    it { should validate_presence_of(:bus_capacity) }
+    it { should validate_uniqueness_of(:bus_code).case_insensitive }
+    it { should allow_value('shohagh-003').for(:bus_code) }
+    it { should_not allow_value('shogagh 001').for(:bus_code) }
+    it { should validate_numericality_of(:bus_capacity).is_greater_than(0).is_less_than_or_equal_to(60) }
+
+  end
 end
