d93f7b1d96d31253cb86ed93d36c68d65b1c2e53
Hasibul Hoque Chowdhury <hasibul.hoque@welldev.io>
2023-04-30 10:39:33 +0600

    Added Shoulda Matchers for testing
    Added shoulda matchers and configured factory bot.

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -5,7 +5,11 @@ require_relative '../config/environment'
 # Prevent database truncation if the environment is production
 abort("The Rails environment is running in production mode!") if Rails.env.production?
 require 'rspec/rails'
+
 # Add additional requires below this line. Rails is not loaded until this point!
+require 'support/factory_bot'
+require 'shoulda/matchers'
+Dir[Rails.root.join('spec','support','**','*rb')].each {  |f| require f }
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
 # spec/support/ and its subdirectories. Files matching `spec/**/*_spec.rb` are
@@ -60,4 +64,10 @@ RSpec.configure do |config|
   config.filter_rails_from_backtrace!
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
+  Shoulda::Matchers.configure do |config|
+    config.integrate do |with|
+      with.test_framework :rspec
+      with.library :rails
+    end
+  end
 end
