cb4dac9f2a6eb9b6d01da5f261e80336bf727ee2
Hasibul Hoque Chowdhury <hasibul.hoque@welldev.io>
2023-04-30 10:41:45 +0600

    Added Tests for User Model
    Added 23 tests for User model.

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,11 +1,10 @@
 FactoryBot.define do
   factory :user do
-    first_name { "MyString" }
-    last_name { "MyString" }
-    email { "MyString" }
-    user_name { "MyString" }
-    encrypted_password { "MyString" }
-    phone { "MyString" }
-    role { 1 }
+    first_name { Faker::Name.first_name }
+    last_name { Faker::Name.last_name }
+    email { Faker::Internet.email }
+    user_name { Faker::Name.first_name }
+    phone { Faker::Base.numerify('+8801#########') }
+    encrypted_password { Faker::Internet.password(min_length: 10, max_length: 20) }
   end
 end
