99a9396d6fb27c5d7e09e3f9ceb74acf04f84771
Hasibul Hoque Chowdhury <hasibul.hoque@welldev.io>
2023-05-01 12:39:08 +0600

    Added Tests for Seat Model
    Added 6 tests for Seat Model.

--- spec/factories/seats.rb
+++ spec/factories/seats.rb
@@ -1,6 +1,6 @@
 FactoryBot.define do
   factory :seat do
-    seat_code { "MyString" }
-    seat_status { 1 }
+    seat_code { [*("A".."Z")].sample + %w[1 2].sample }
+    seat_status { %i[seat_available on_hold booked].sample }
   end
 end
--- spec/models/seat_spec.rb
+++ spec/models/seat_spec.rb
@@ -1,5 +1,15 @@
-require 'rails_helper'
+require "rails_helper"
 
 RSpec.describe Seat, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:seat) { FactoryBot.build(:seat) }
+  describe "when creating a seat" do
+    it "has a valid factory" do
+      expect(seat).to be_valid
+    end
+    it { should validate_presence_of(:seat_code) }
+    it { should validate_presence_of(:seat_status) }
+    it { should allow_value("A3").for(:seat_code) }
+    it { should_not allow_value("A 3").for(:seat_code) }
+    it { should_not allow_value("").for(:seat_code) }
+  end
 end
