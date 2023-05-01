d140a12bb1ec537f724c794066544db395e51ded
Hasibul Hoque Chowdhury <hasibul.hoque@welldev.io>
2023-05-01 15:25:16 +0600

    Added Test for Ticket Model
    Added 2 tests for Ticket Model

--- spec/factories/tickets.rb
+++ spec/factories/tickets.rb
@@ -1,5 +1,5 @@
 FactoryBot.define do
   factory :ticket do
-    total_fare { 1 }
+    total_fare { Faker::Number.between(from: 1, to: 1000) }
   end
 end
--- spec/models/ticket_spec.rb
+++ spec/models/ticket_spec.rb
@@ -1,5 +1,11 @@
-require 'rails_helper'
+require "rails_helper"
 
 RSpec.describe Ticket, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:ticket) { FactoryBot.build(:ticket) }
+  describe "when creating a ticket" do
+    it "has a valid factory" do
+      expect(ticket).to be_valid
+    end
+    it { should validate_presence_of(:total_fare) }
+  end
 end
