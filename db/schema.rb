bece57ebc7710e56c2e8b168b7c70626a563cbd6
Hasibul Hoque Chowdhury <hasibul.hoque@welldev.io>
2023-05-01 11:24:22 +0600

    Payment Model created
    Created Payment model and migrated the database.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_05_01_043649) do
+ActiveRecord::Schema[7.0].define(version: 2023_05_01_052322) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -19,6 +19,11 @@ ActiveRecord::Schema[7.0].define(version: 2023_05_01_043649) do
     t.integer "bus_type", default: 0, null: false
     t.string "bus_company", null: false
     t.integer "bus_capacity", default: 0, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "payments", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
