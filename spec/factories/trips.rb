e186d791d47db219c60c5f8f1662ac85cb84391a
Hasibul Hoque Chowdhury <hasibul.hoque@welldev.io>
2023-05-01 11:19:35 +0600

    Added Tests for Trip model
    Added 6 tests for Trip model.

--- spec/factories/trips.rb
+++ spec/factories/trips.rb
@@ -1,8 +1,8 @@
 FactoryBot.define do
   factory :trip do
-    ticket_price { 1 }
-    total_booked { 1 }
-    trip_date { "2023-05-01" }
-    trip_time { "2023-05-01 10:36:49" }
+    ticket_price { Faker::Number.between(from: 1, to: 10_000) }
+    total_booked { Faker::Number.between(from: 1, to: 60) }
+    trip_date { Faker::Date.between(from: "2023-05-01", to: "2024-05-01") }
+    trip_time { Faker::Time.between(from: DateTime.now - 1, to: DateTime.now, format: :default) }
   end
 end
--- spec/models/trip_spec.rb
+++ spec/models/trip_spec.rb
@@ -1,5 +1,17 @@
-require 'rails_helper'
+require "rails_helper"
 
 RSpec.describe Trip, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:trip) { FactoryBot.build(:trip) }
+  describe "when creating a trip" do
+    it "has a valid factory" do
+      expect(trip).to be_valid
+    end
+    it { should validate_presence_of(:ticket_price) }
+    it { should validate_presence_of(:total_booked) }
+    it { should validate_presence_of(:trip_date) }
+    it { should validate_presence_of(:trip_time) }
+    it {
+      should validate_numericality_of(:ticket_price).is_greater_than(0).is_less_than_or_equal_to(10_000)
+    }
+  end
 end
