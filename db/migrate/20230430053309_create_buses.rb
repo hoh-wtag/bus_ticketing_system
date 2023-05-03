e0348ee5efe356d683c384744126017fdcd2663c
Hasibul Hoque Chowdhury <hasibul.hoque@welldev.io>
2023-05-03 20:08:20 +0600

    removed extra unique: true in create bus migration.

--- db/migrate/20230430053309_create_buses.rb
+++ db/migrate/20230430053309_create_buses.rb
@@ -1,7 +1,7 @@
 class CreateBuses < ActiveRecord::Migration[7.0]
   def change
     create_table :buses do |t|
-      t.string :bus_code, null:false, unique: true
+      t.string :bus_code, null:false
       t.integer :bus_type, null: false, default: 0
       t.string :bus_company, null:false
       t.integer :bus_capacity, null: false, default: 0
