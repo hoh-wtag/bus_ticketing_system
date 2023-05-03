f270c6a63fe5de09cdeea7c646254605b9e7a0b9
Hasibul Hoque Chowdhury <hasibul.hoque@welldev.io>
2023-05-03 11:25:48 +0600

    Added after_create Callback for Trip model
    Added after_create Callback for trip model so that create_seats function runs every time an object is saved.

--- app/models/trip.rb
+++ app/models/trip.rb
@@ -8,4 +8,12 @@ class Trip < ApplicationRecord
 
   validates :ticket_price, :total_booked, :trip_date, :trip_time, presence: true
   validates :ticket_price, numericality: { greater_than: 0, less_than_or_equal_to: 10_000 }
+
+  after_create :create_seats
+
+  private
+
+  def create_seats
+    (0..bus.bus_capacity - 1).each { |a| Seat.create(seat_code: "#{((a / 4) + 65).chr}#{(a % 4) + 1}", seat_status: 0, trip_id: id) }
+  end
 end
