d17b4d33e44b85eeb262d365efd92d67ce920371
Hasibul Hoque Chowdhury <hasibul.hoque@welldev.io>
2023-05-07 13:59:53 +0600

    Created Controller and View for Bus Model

---
+++ app/controllers/buses_controller.rb
@@ -0,0 +1,45 @@
+class BusesController < ApplicationController
+  before_action :show, only: %i[edit update destroy]
+
+  def index
+    @buses = Bus.all
+  end
+
+  def new
+    @bus = Bus.new
+  end
+
+  def edit; end
+
+  def create
+    @bus = Bus.new(bus_params)
+    if @bus.save
+      redirect_to action: "index", status: :created
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def update
+    if @bus.update(bus_params)
+      redirect_to action: "index", status: :see_other
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @bus.destroy
+    redirect_to action: "index", status: :see_other
+  end
+
+  private
+
+  def bus_params
+    params.require(:bus).permit(:code, :bus_type, :company, :capacity)
+  end
+
+  def show
+    @bus = Bus.find(params[:id])
+  end
+end
