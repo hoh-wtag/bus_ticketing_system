49e5ec7a4519a8d989453f0194c4bbcd534f3d77
Hasibul Hoque Chowdhury <hasibul.hoque@welldev.io>
2023-05-17 18:10:22 +0600

    used helper method for current user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,17 +1,17 @@
 class ApplicationController < ActionController::Base
-  before_action :set_current_user
+  include ApplicationHelper
 
   def set_current_user
     return unless session[:user_id]
 
-    Current.user = User.find_by(id: session[:user_id])
+    current_user
   end
 
   def require_user_signed_in
-    redirect_to signin_path, alert: t(:need_signup_for_access) if Current.user.nil?
+    redirect_to signin_path, alert: t(:need_signup_for_access) if current_user.nil?
   end
 
   def already_signed_in
-    redirect_to root_path, alert: t(:user_already_signed_in) unless Current.user.nil?
+    redirect_to root_path, alert: t(:user_already_signed_in) unless current_user.nil?
   end
 end
