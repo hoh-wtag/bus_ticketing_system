255534550e4afe1918a47cfb4bf667c9ac54f012
Hasibul Hoque Chowdhury <hasibul.hoque@welldev.io>
2023-05-31 09:56:03 +0600

    Updated Trips FactoryBot

--- spec/factories/trips.rb
+++ spec/factories/trips.rb
@@ -2,7 +2,6 @@ FactoryBot.define do
   factory :trip do
     ticket_price { Faker::Number.between(from: 1, to: 10_000) }
     date { Faker::Date.between(from: '2023-05-01', to: '2024-05-01') }
-    total_booked { Faker::Number.between(from: 1, to: 60) }
     time { Faker::Time.between(from: DateTime.now - 1, to: DateTime.now, format: :default) }
 
     bus
